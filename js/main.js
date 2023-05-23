b487a4d034553004f63b752b28a18a4e31023e60
gpr-xirdalan <134236732+gpr-xirdalan@users.noreply.github.com>
2023-05-23 20:52:26 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
+
 $(document).ready(function(){
-    $('.btn').click(function() {
 
-        $('.preloader').removeClass('hide');
-
+    // очищаем результат вывода
+    function reset_result() {
         $('.result-list').html('');
+    }
 
-        let value = $('.input').val();
+    // показывает прелоадер
+    function show_preloader() {
+        $('.preloader').removeClass('hide');
+        
+    }
+    
+    // скрываем прелоадер
+    function hide_preloader() {
+        $('.preloader').addClass('hide');
+    }
+
+    // поиск по старым названиям товара
+    function search_products_name(value) {
+        show_preloader();
+
+        reset_result();
+
         value = new RegExp(value, "i");
-        $.getJSON('js/ADLARI_DEYISEN_MEHSULLAR.json', function(data){
+
+        $.getJSON('js/ADLARI_DEYISEN_MEHSULLAR.json', function(data) {
             $.each(data, function(key, val){
-                if((val['MƏHSULUN KOHNE ADI'].search(value) != -1)) {
+                if((val['MƏHSULUN KOHNE ADI'].search(value) != -1) || (val['MƏHSULUN YENI ADI'].search(value) != -1) ) {
                     
                     $('.result-list').append(
                     `
@@ -32,12 +50,63 @@ $(document).ready(function(){
                     
                     `
                     );
+
+                    return;
                 }
             });
 
-            $('.preloader').addClass('hide');
-
+            hide_preloader();
         });
 
+    }
+
+
+    // поиск по своместимости защитных стекол
+    function search_glass(value) {
+
+        show_preloader();
+
+        reset_result();
+
+        value = new RegExp(value, "i");
+        let arr = [];
+
+        $.getJSON('js/glass.json', function(data) {
+            $.each(data, function(key, val){
+                $.each( val, function( glass_id, glass_name ) {
+                    if (glass_name.search(value) != -1) {
+                        val.filter((eVal) =>  {
+                            $('.result-list').append(
+                                `
+                            <div class="result-item-u">
+                                <span class="result-item-text old-name">${eVal}</span>
+                            </div>                  
+                                
+                                `
+                                );
+                        });
+                        return;
+                    }
+                  });
+            });
+
+            hide_preloader();
+        });
+    }
+
+
+    $('.search-glass').on('click', function() {
+        var value = $('.input').val();
+
+        search_glass(value);
     });
+
+
+
+    $('.search-products-name').click(function() {
+        let value = $('.input').val();
+
+        search_products_name(value);
+    });
+
 });
