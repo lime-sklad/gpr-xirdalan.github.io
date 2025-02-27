ab0d1b57636eb8f31b175b3c627249aafc1ff940
gpr-xirdalan <134236732+gpr-xirdalan@users.noreply.github.com>
2025-02-27 20:37:49 +0400

    Update function.js

--- js/function.js
+++ js/function.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     let currentIndex = 0;
     let isLoading = false;
 
-    $.getJSON("products.json?v=100", function(data) {
+    $.getJSON("products.json?v=101", function(data) {
       products = data.products;
       getCategoryList();
 
