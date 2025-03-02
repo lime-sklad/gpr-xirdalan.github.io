5e17f04e0306d6a9de9f15a07a10da94edc48daa
Emil <emil.gasanovua@gmail.com>
2025-03-03 01:23:38 +0400

    fixed

--- api/saveOrder.js
+++ api/saveOrder.js
@@ -1,4 +1,13 @@
 export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // Разрешить запросы от всех доменов
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        res.status(200).end();
+        return;
+    }
+    
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Метод не поддерживается" });
     }
