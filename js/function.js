c8eb1b7c3af233191ef98a7ec53d4bf4e8a58ef6
Emil <emil.gasanovua@gmail.com>
2025-04-03 00:12:29 +0400

    s

--- js/function.js
+++ js/function.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
     let isLoading = false;
     let customData = [];
 
-    $.getJSON("products.json?v=119", function(data) {
+    $.getJSON("products.json?v=120", function(data) {
       products = data.products;
       getCategoryList();
 
