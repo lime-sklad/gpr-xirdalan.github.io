9c7d2afdb5809f30a4d903fcacddb658640696a4
Emil <myfakemailforregsite20@gmail.com>
2025-06-27 18:20:31 +0400

    ВЫ

--- js/function.js
+++ js/function.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     let isLoading = false;
     let customData = [];
 
-    $.getJSON("products.json?v=131", function(data) {
+    $.getJSON("products.json?v=132", function(data) {
       products = data.products;
       getCategoryList();
 
